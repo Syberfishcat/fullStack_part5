a04ef2a8678f6ed9252b1ee9e835fe5cbf85db32
sybercat <sybercat@163.com>
2026-09-02 13:22:25 +0300

    complete 5.14

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -25,4 +26,29 @@ test('renders content', () => {
 
   element = screen.getByText('like')
   expect(element).not.toBeVisible()
+})
+
+test('clicking the button ccalls event handler once', async () => {
+const blog =  {
+    title: 'Blog renders test',
+    author: 'yqs',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+    user: {
+        username: 'tester',
+        name: 'testName'
+    }
+  }
+
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  let element = screen.getByText('https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf')
+  expect(element).toBeVisible()
+
+  element = screen.getByText('like')
+  expect(element).toBeVisible()
 })
